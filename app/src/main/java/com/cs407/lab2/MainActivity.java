2fded97568e734c5b58aa514c1ccbe7e692979c3
amelia <meyeaz@gmail.com>
2023-09-25 04:30:56 -0500

    4 - milestone 1 checkpoint

--- app/src/main/java/com/cs407/lab2/MainActivity.java
+++ app/src/main/java/com/cs407/lab2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.cs407.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,12 +14,19 @@ public class MainActivity extends AppCompatActivity {
     public void clickFunction(View view) {
         EditText myTextField = (EditText) findViewById(R.id.myTextField);
         Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+        goToActivity(myTextField.getText().toString());
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToActivity(String s){
+        Intent intent = new Intent(this, CalculatorActivity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
     }
 
 }
